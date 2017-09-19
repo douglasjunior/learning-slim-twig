0f57e2e3df2aa6bd6be9e483f73eb6aec9e270db
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-09-19 11:21:03 -0300

    no message

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Controllers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class HomeController
 {
 
-    function index($request, $response)
+    function index(Request $request, Response $response)
     {
         return 'Home Controller';
     }
